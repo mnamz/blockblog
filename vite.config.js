65c0d756c22b1483bb83ccedfaf9423a35a69ebc
Aliif Zin <aliif.zin@osky.com.au>
2023-11-18 21:36:43 +0800

    add sass

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: ['resources/js/app.js', 'resources/sass/app.scss'],
             ssr: 'resources/js/ssr.js',
             refresh: true,
         }),
@@ -18,5 +18,4 @@ export default defineConfig({
             },
         }),
     ],
-    envDir: 'src',
 });
