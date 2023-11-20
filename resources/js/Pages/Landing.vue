e662fa7b7aa165eb6ff9575b364b0033eeabfbae
Syed <fakrullah.fauzi@osky.com.au>
2023-11-20 11:49:55 +0800

    homepage: progress

--- resources/js/Pages/Landing.vue
+++ resources/js/Pages/Landing.vue
@@ -1,12 +1,15 @@
 <script setup>
-import { Head, Link } from "@inertiajs/vue3"
-import buttonPrimary from "../Components/Portfolio/textButton.vue"
-import githubIcon from "../Components/Portfolio/svg/github.vue"
-import linkedinIcon from "../Components/Portfolio/svg/linkedin.vue"
-import mailIcon from "../Components/Portfolio/svg/mail.vue"
-import phoneIcon from "../Components/Portfolio/svg/phone.vue"
-import newtabSmIcon from "../Components/Portfolio/svg/newtab-sm.vue"
-import blogarrowIcon from "../Components/Portfolio/svg/blogArrow.vue"
+import { Head, Link } from "@inertiajs/vue3";
+import buttonPrimary from "../Components/Portfolio/textButton.vue";
+import githubIcon from "../Components/Portfolio/svg/github.vue";
+import linkedinIcon from "../Components/Portfolio/svg/linkedin.vue";
+import mailIcon from "../Components/Portfolio/svg/mail.vue";
+import phoneIcon from "../Components/Portfolio/svg/phone.vue";
+import newtabSmIcon from "../Components/Portfolio/svg/newtab-sm.vue";
+import newtablgIcon from "../Components/Portfolio/svg/newtab-lg.vue";
+import blogarrowIcon from "../Components/Portfolio/svg/blogArrow.vue";
+import prevarrowIcon from "../Components/Portfolio/svg/prevArrow.vue";
+import nextarrowIcon from "../Components/Portfolio/svg/nextArrow.vue";
 
 defineProps({
     canLogin: Boolean,
@@ -20,41 +23,42 @@ defineProps({
     <Head title="Aliif Zin" />
 
     <div
-        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-grid bg-center bg-gray-100 dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-
-        <div class="container max-940 px-3 lg:px-0">
+        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-grid bg-center bg-gray-100 dark:bg-gray-900 selection:bg-red-500 selection:text-white"
+    >
+        <div class="max-940 px-3 lg:px-0">
             <!-- Logo -->
             <div class="w-full text-center py-5">
                 <a href="" class="az-logo text-2xl font-bold">Az.</a>
             </div>
 
-            <div class="carousel">
-                <div><img src="https://picsum.photos/300/200?random=1"></div>
-                <div><img src="https://picsum.photos/300/200?random=2"></div>
-                <div><img src="https://picsum.photos/300/200?random=3"></div>
-                <div><img src="https://picsum.photos/300/200?random=4"></div>
-                <div><img src="https://picsum.photos/300/200?random=5"></div>
-                <div><img src="https://picsum.photos/300/200?random=6"></div>
-            </div>
-
-
             <!-- Main content -->
             <div class="grid grid-cols-2 sm:grid-cols-4 sm:grid-rows-6 gap-5">
-
                 <!-- Hero Section -->
-                <div class="panel col-span-2 sm:col-span-3 sm:row-span-3 flex flex-col sm:flex-row-reverse justify-between gap-5">
+                <div
+                    class="panel col-span-2 sm:col-span-3 sm:row-span-3 flex flex-col sm:flex-row-reverse justify-between gap-5"
+                >
                     <div class="header-img sm:w-2/6">
-                        <img class="rounded-lg h-full w-full object-cover overflow-visible" src="../../../storage/images/memojiAlep.png" alt="A picture of me">
+                        <img
+                            class="rounded-lg h-full w-full object-cover overflow-visible"
+                            src="../../../storage/images/memojiAlep.png"
+                            alt="A picture of me"
+                        />
                     </div>
-                    <div class="hero-content sm:w-4/6 flex h-full flex-col justify-between gap-3 sm:gap-16">
+                    <div
+                        class="hero-content sm:w-4/6 flex h-full flex-col justify-between gap-3 sm:gap-16"
+                    >
                         <div>
                             <p class="panel-label text-label">01 welcome</p>
-                            <p class="text-2xl text-white">Hi! I'm Aliif Zin, a software developer based in Selangor, Malaysia.
-                                Dedicated to crafting robust, user-centric applications that ace the A to Z
-                                requirements.
+                            <p class="text-2xl text-white">
+                                Hi! I'm Aliif Zin, a software developer based in
+                                Selangor, Malaysia. Dedicated to crafting
+                                robust, user-centric applications that ace the A
+                                to Z requirements.
                             </p>
                         </div>
-                        <div class="header-cta flex flex-row items-center gap-2">
+                        <div
+                            class="header-cta flex flex-row items-center gap-2"
+                        >
                             <buttonPrimary :buttonText="'resume'" />
                             <button class="icon-button p-1.5 rounded-lg">
                                 <githubIcon />
@@ -73,75 +77,200 @@ defineProps({
                 </div>
 
                 <!-- Contact Section -->
-                <div class="panel sm:col-span-1 sm:row-span-3 flex flex-col gap-5">
+                <div
+                    class="panel sm:col-span-1 sm:row-span-3 flex flex-col gap-5"
+                >
                     <div class="flex flex-col">
                         <p class="panel-label text-label">03 contacts</p>
-                        <p class="text-base text-white font-bold">Got something to say or enquiries?</p>
+                        <p class="text-base text-white font-bold">
+                            Got something to say or enquiries?
+                        </p>
                     </div>
                     <div class="flex flex-col gap-3.5">
                         <p class="text-xs italic text-grey">Contact details</p>
                         <div class="flex flex-row gap-2 items-center">
-                            <mailIcon/>
-                            <a class="normal-link text-white" href="mailto:">mnamz101@gmail.com</a>
+                            <mailIcon />
+                            <a class="normal-link text-white" href="mailto:"
+                                >mnamz101@gmail.com</a
+                            >
                         </div>
                         <div class="flex flex-row gap-2 items-center">
-                            <phoneIcon/>
-                            <a class="normal-link text-white" href="mailto:">0123456789</a>
+                            <phoneIcon />
+                            <a class="normal-link text-white" href="mailto:"
+                                >0123456789</a
+                            >
                         </div>
                     </div>
                     <div class="flex flex-col gap-.5">
                         <p class="text-xs italic text-grey">Socials</p>
-                        <p><a class="arrow-link text-white" href="#">Linkedin</a></p>
-                        <p><a class="arrow-link text-white" href="#">Instagram</a></p>
-                        <p><a class="arrow-link text-white" href="#">Twitter</a></p>
-                        <p><a class="arrow-link text-white" href="#">Discord</a></p>
-                        <p><a class="arrow-link text-white" href="#">Jobstreet</a></p>
+                        <p>
+                            <a class="arrow-link text-white" href="#"
+                                >Linkedin</a
+                            >
+                        </p>
+                        <p>
+                            <a class="arrow-link text-white" href="#"
+                                >Instagram</a
+                            >
+                        </p>
+                        <p>
+                            <a class="arrow-link text-white" href="#"
+                                >Twitter</a
+                            >
+                        </p>
+                        <p>
+                            <a class="arrow-link text-white" href="#"
+                                >Discord</a
+                            >
+                        </p>
+                        <p>
+                            <a class="arrow-link text-white" href="#"
+                                >Jobstreet</a
+                            >
+                        </p>
                     </div>
                 </div>
 
                 <!-- Projects -->
-                <div class="panel col-span-2 sm:row-span-2">
-                    <p class="panel-label text-label">04 projects</p>
+                <div class="project-wrapper relative col-span-2 sm:row-span-2">
+                    <div
+                        class="carousel-control absolute z-10 w-full flex justify-center items-center mt-2.5 gap-2.5"
+                    >
+                        <button
+                            class="carousel-button prevBtn bg-dark p-1.5 rounded-lg"
+                        >
+                            <prevarrowIcon />
+                        </button>
+                        <p
+                            class="text-base project-label bg-dark py-0.5 px-2 mb-0 rounded-lg"
+                        >
+                            04 Projects
+                        </p>
+                        <button
+                            class="carousel-button nextBtn bg-dark p-1.5 rounded-lg"
+                        >
+                            <nextarrowIcon />
+                        </button>
+                    </div>
+                    <div class="project-carousel overflow-hidden rounded-lg">
+                        <div class="item relative">
+                            <img src="../../../storage/images/Tintedprojectimage.png" alt="">
+                            <div class="item-title absolute z-10 flex flex-row gap-3 items-center">
+                                <p class="text-white text-2xl font-bold">Moad</p>
+                                <a href="">
+                                    <newtablgIcon/>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="item relative">
+                            <img src="../../../storage/images/Tintedprojectimage.png" alt="">
+                            <div class="item-title absolute z-10 flex flex-row gap-3 items-center">
+                                <p class="text-white text-2xl font-bold">Moad2</p>
+                                <a href="">
+                                    <newtablgIcon/>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="item relative">
+                            <img src="../../../storage/images/Tintedprojectimage.png" alt="">
+                            <div class="item-title absolute z-10 flex flex-row gap-3 items-center">
+                                <p class="text-white text-2xl font-bold">Moad3</p>
+                                <a href="">
+                                    <newtablgIcon/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
                 <!-- Tech Stack Section -->
-                <div class="panel sm:col-span-1">
-                    <p class="panel-label text-label">07 tech stack</p>
+                <div class="panel p-0 sm:col-span-1">
+                    <p class="panel-label text-label p-2.5">07 tech stack</p>
+                    <div class="techStack-carousel py-3">
+                        <div class="px-2">
+                            <img
+                                src="../../../storage/images/Cloud.png"
+                                alt=""
+                            />
+                        </div>
+                        <div class="px-2">
+                            <img
+                                src="../../../storage/images/Drupal.png"
+                                alt=""
+                            />
+                        </div>
+                        <div class="px-2">
+                            <img
+                                src="../../../storage/images/Flutter.png"
+                                alt=""
+                            />
+                        </div>
+                        <div class="px-2">
+                            <img src="../../../storage/images/Fz.png" alt="" />
+                        </div>
+                        <div class="px-2">
+                            <img src="../../../storage/images/Git.png" alt="" />
+                        </div>
+                        <div class="px-2">
+                            <img
+                                src="../../../storage/images/Jquery.png"
+                                alt=""
+                            />
+                        </div>
+                    </div>
                 </div>
 
                 <!-- Blog Section -->
                 <div class="panel sm:col-span-1 sm:row-span-2">
-                    <div class="flex flex-row justify-between items-center mb-1">
+                    <div
+                        class="flex flex-row justify-between items-center mb-1"
+                    >
                         <p class="panel-label text-label mb-0">08 blog</p>
-                        <a href=""><newtabSmIcon/></a>
+                        <a href="" class="newtabIcon"><newtabSmIcon /></a>
                     </div>
-                    <div class="blog-listing overflow-y-scroll blog-overflow pr-2">
+                    <div
+                        class="blog-listing overflow-y-scroll blog-overflow pr-2"
+                    >
                         <div class="blog-item flex flex-col gap-1 pt-2">
-                            <p class="text-sm text-white">Contoh-contoh Rizz 13 May digunakan dalam masyar...</p>
-                            <div class="flex flex-row justify-between items-center">
+                            <p class="text-white">
+                                <a class="normal-link" href="#"
+                                    >Contoh-contoh Rizz 13 May digunakan dalam
+                                    masyar...</a
+                                >
+                            </p>
+                            <div
+                                class="flex flex-row justify-between items-center"
+                            >
                                 <p class="text-xs text-grey">13 May 2020</p>
-                                <blogarrowIcon/>
+                                <a href="#"><blogarrowIcon /></a>
                             </div>
                         </div>
                         <div class="blog-item flex flex-col gap-1 pt-2">
-                            <p class="text-sm text-white">Contoh-contoh Rizz 13 May digunakan dalam masyar...</p>
-                            <div class="flex flex-row justify-between items-center">
+                            <p class="text-white">
+                                <a class="normal-link" href="#"
+                                    >Contoh-contoh Rizz 13 May digunakan dalam
+                                    masyar...</a
+                                >
+                            </p>
+                            <div
+                                class="flex flex-row justify-between items-center"
+                            >
                                 <p class="text-xs text-grey">13 May 2020</p>
-                                <blogarrowIcon/>
+                                <a href="#"><blogarrowIcon /></a>
                             </div>
                         </div>
                         <div class="blog-item flex flex-col gap-1 pt-2">
-                            <p class="text-sm text-white">Contoh-contoh Rizz 13 May digunakan dalam masyar...</p>
-                            <div class="flex flex-row justify-between items-center">
+                            <p class="text-white">
+                                <a class="normal-link" href="#"
+                                    >Contoh-contoh Rizz 13 May digunakan dalam
+                                    masyar...</a
+                                >
+                            </p>
+                            <div
+                                class="flex flex-row justify-between items-center"
+                            >
                                 <p class="text-xs text-grey">13 May 2020</p>
-                                <blogarrowIcon/>
-                            </div>
-                        </div>
-                        <div class="blog-item flex flex-col gap-1 pt-2">
-                            <p class="text-sm text-white">Contoh-contoh Rizz 13 May digunakan dalam masyar...</p>
-                            <div class="flex flex-row justify-between items-center">
-                                <p class="text-xs text-grey">13 May 2020</p>
-                                <blogarrowIcon/>
+                                <a href="#"><blogarrowIcon /></a>
                             </div>
                         </div>
                     </div>
@@ -153,30 +282,20 @@ defineProps({
                         <p class="panel-label text-label">05 status</p>
                         <div class="blob"></div>
                     </div>
-                    <p class="text-white text-sm font-light">Software developer at Osky</p>
+                    <p class="text-white text-sm font-light">
+                        Software developer at Osky
+                    </p>
                 </div>
-                
+
                 <!-- Footer Section -->
                 <div class="panel col-span-2 sm:col-span-1 sm:row-span-1">
                     <p class="panel-label text-label">06 footer</p>
-                    <p class="text-white text-sm font-light">© 2023 · Assembled carefully using Tailwind by Aliif Zin.</p>
+                    <p class="text-white text-sm font-light">
+                        © 2023 · Assembled carefully using Tailwind by Aliif
+                        Zin.
+                    </p>
                 </div>
             </div>
         </div>
     </div>
 </template>
-
-<style>
-.bg-grid {
-    background-color: #001120;
-    opacity: 1;
-    background-image: linear-gradient(rgba(255, 255, 255, 0.1) 1.3px,
-            transparent 1.3px),
-        linear-gradient(to right, rgba(255, 255, 255, 0.1) 1.3px, #001120 1.3px);
-    background-size: 26px 26px;
-}
-
-.max-940 {
-    max-width: 940px;
-}
-</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,52 +1,40 @@
-import './bootstrap';
-import '../css/app.css';
+import "./bootstrap";
+import "../css/app.css";
 
-import {
-    createApp,
-    h
-} from 'vue';
-import {
-    createInertiaApp
-} from '@inertiajs/vue3';
-import {
-    resolvePageComponent
-} from 'laravel-vite-plugin/inertia-helpers';
-import {
-    ZiggyVue
-} from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy/dist/vue.m";
 
-const appName =
-    import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`,
-        import.meta.glob('./Pages/**/*.vue')),
-    setup({
-        el,
-        App,
-        props,
-        plugin
-    }) {
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob("./Pages/**/*.vue")
+        ),
+    setup({ el, App, props, plugin }) {
         return createApp({
-                render: () => h(App, props)
-            })
+            render: () => h(App, props),
+        })
             .use(plugin)
             .use(ZiggyVue)
             .mount(el);
     },
     progress: {
-        color: '#4B5563',
+        color: "#4B5563",
     },
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-    console.log('script loaded')
+    console.log("script loaded");
     setTimeout(function () {
-        const panels = document.querySelectorAll('.panel');
+        const panels = document.querySelectorAll(".panel");
 
-        panels.forEach(panel => {
-            panel.addEventListener('mousemove', (e) => {
+        panels.forEach((panel) => {
+            panel.addEventListener("mousemove", (e) => {
                 const rect = panel.getBoundingClientRect();
 
                 const left = e.pageX - rect.left;
@@ -56,5 +44,27 @@ document.addEventListener("DOMContentLoaded", function () {
                 panel.style.setProperty("--top", `${top}px`);
             });
         });
-    }, 500);
+
+        $(".techStack-carousel").slick({
+            infinite: true,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 0,
+            pauseOnHover: false,
+            speed: 2000,
+            nextArrow: false,
+            prevArrow: false,
+            cssEase: "linear",
+        });
+
+        $(".project-carousel").slick({
+            infinite: true,
+            dots: false,
+            autoplay: true,
+            autoplaySpeed: 1500,
+            prevArrow: $(".prevBtn"),
+            nextArrow: $(".nextBtn"),
+        });
+    }, 300);
 });
