9b59126453a8b1bb38e2289ecde7e72fcb32f0d4
Syed Ahmad Fakrullah <syedahmadfakhrullah@gmail.com>
2023-11-22 23:29:51 +0800

    added and updated blog listing page

---
+++ resources/js/Components/Portfolio/Blog-list.vue
@@ -0,0 +1,88 @@
+<script setup>
+import newtabSm from './svg/newtab-sm.vue';
+import githubIcon from './svg/github.vue';
+import Linkedin from './svg/linkedin.vue';
+import telegram from './svg/telegram.vue';
+import Discord from './svg/discord.vue';
+import instagram from './svg/instagram.vue';
+</script>
+
+<template>
+    <div class="flex flex-col sm:flex-row gap-2.5">
+
+        <div class="w-full sm:w-2/4">
+            <div class="flex items-center gap-2.5 mb-4">
+                <div class="panel text-label m-0">01 Blog listing</div>
+                <p class="text-white text-xs">I be saying stuff lately...</p>
+            </div>
+            <div class="flex flex-col gap-2 5">
+                <!-- Blog-items -->
+                <div class="blog-item panel flex flex-col gap-2 w-full">
+                    <div class="flex justify-between items-center">
+                        <p class="text-grey text-xs">11 May 2032</p>
+                        <a class="newtabIcon" href="">
+                            <newtabSm />
+                        </a>
+                    </div>
+                    <p class="text-base font-medium text-white">Title for the blog item</p>
+                    <p class="text-grey text-xs">Description of the shit you be saying lately with ...</p>
+                </div>
+
+            </div>
+
+        </div>
+        <div class="w-full sm:w-2/4 sm:relative">
+            <div class="sm:sticky sm:top-2.5">
+                <div class="panel inline-flex items-center justify-center gap-2.5 sm:float-right mb-2.5">
+                    <a class="panel-icon" href="">
+                        <githubIcon />
+                    </a>
+
+                    <a class="panel-icon" href="">
+                        <Linkedin />
+                    </a>
+
+                    <a class="panel-icon" href="">
+                        <telegram />
+                    </a>
+
+                    <a class="panel-icon" href="">
+                        <instagram />
+                    </a>
+
+                    <a class="panel-icon" href="">
+                        <discord />
+                    </a>
+                </div>
+
+                <div class="add-info panel flex flex-col sm:float-right">
+                    <div class="text-label">02 Additional information</div>
+                    <div class="flex flex-col gap-4">
+                        <p class="text-base text-white font-light">What I used for updating all this content?</p>
+                        <div class="flex flex-col gap-0">
+                            <p class="text-xs italic text-grey">CMS</p>
+                            <p><a class="normal-link text-red font-normal" href="#">BlockBlog</a></p>
+                            <p><a class="normal-link text-red" href="#">Laravel</a></p>
+                        </div>
+                        <div class="flex flex-col gap-0">
+                            <p class="text-xs italic text-grey">Frontend</p>
+                            <p><a class="normal-link text-red font-normal" href="#">Tailwind CSS</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    components: { telegram, Discord },
+    methods: {
+        getFullPath(relativePath) {
+            return process.env.BASE_URL + relativePath;
+        }
+    }
+}
+</script>
--- resources/js/Pages/Landing.vue
+++ resources/js/Pages/Landing.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { Head, Link } from "@inertiajs/vue3";
 import Landing from "@/Components/Portfolio/Landing.vue";
+import BlogList from "@/Components/Portfolio/Blog-list.vue";
 
 defineProps({
     canLogin: Boolean,
@@ -12,17 +13,18 @@ defineProps({
 
 <template>
     <Head title="Aliif Zin" />
-    <!-- logo -->
-    <div class="w-full text-center py-5">
-        <a href="" class="az-logo text-2xl font-bold">Az.</a>
-    </div>
     <div
-        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-grid bg-center bg-gray-100 dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-        <div class="custom-container px-3 lg:px-0">
-            <!-- Main content -->
-            <div class="grid grid-cols-2 sm:grid-cols-4 sm:grid-rows-6 gap-5">
-                <Landing :canLogin="canLogin" :canRegister="canRegister" :laravelVersion="laravelVersion" :phpVersion="phpVersion" />
+        class="relative sm:flex flex-col sm:justify-start sm:items-center min-h-screen bg-center selection:bg-red-500 selection:text-white">
+        <div class="custom-container w-full px-3 lg:px-0">
+            <!-- logo -->
+            <div class="w-full text-center py-5">
+                <a href="" class="az-logo text-2xl font-bold">az.</a>
             </div>
+            <!-- Main content -->
+            <BlogList :canLogin="canLogin" :canRegister="canRegister" :laravelVersion="laravelVersion"
+                :phpVersion="phpVersion" />
+            <!-- <Landing :canLogin="canLogin" :canRegister="canRegister" :laravelVersion="laravelVersion"
+                    :phpVersion="phpVersion" /> -->
         </div>
     </div>
 </template>
